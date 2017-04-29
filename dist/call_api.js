a89204984c0aa3db782b5b9a528c4e7d62e2f31e
shunkin <shunkin@sfc.wide.ad.jp>
2017-04-30 06:57:22 +0900

    wrote some util and, fixed small bug.

--- dist/call_api.js
+++ dist/call_api.js
@@ -1,6 +1,5 @@
 var host = "https://salespause-phone.au-syd.mybluemix.net"
 
-arrival();
 
 function arrival() {
 	var http = new XMLHttpRequest();
@@ -11,7 +10,16 @@ function arrival() {
 };
 
 function change(type) {
-	xmlhttp.open("POST", url);
-	xmlhttp.setRequestHeader("x-filename", photoId);
-	xmlhttp.send(formData);
-}
+	var http = new XMLHttpRequest();
+	typeData = ["friend", "neighbor", "danger", "random"]
+	if (type < 4) {
+		console.log(typeData[type])
+		typePost = "type=" + typeData[type]
+	} else {
+		typePost = "type=none"
+	}
+	http.open("post", host + "/status/types", true);
+	http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	http.setRequestHeader("auth-secret", "dekitango");
+	http.send(typePost);
+};
